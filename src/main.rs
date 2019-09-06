b2dba35a61b3c0796bd23309b966eb08e871a631
tarberd <bernardo.mferrari@gmail.com>
2019-09-06 14:40:55 -0300

    add simple xorshift

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+fn xorshift(seed: u32) -> u32 {
+    let x = seed;
+
+    let x = x ^ (x << 13);
+    let x = x ^ (x >> 17);
+    let x = x ^ (x << 5);
+    x
+}
+
 fn main() {
-    println!("Hello, world!");
+    let seed = 123413241u32;
+
+    let seed = xorshift(seed);
+    println!("Random number: {:032b}", seed);
+    let seed = xorshift(seed);
+    println!("Random number: {:032b}", seed);
 }
