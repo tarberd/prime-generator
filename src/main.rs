443a9479865bf579cae7ec3229f6307ab3f1566f
tarberd <bernardo.mferrari@gmail.com>
2019-09-09 16:04:18 -0300

    First miller-rabin draft

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,26 @@ fn xorshift(seed: u32) -> u32 {
 fn linear_congruential(seed: u32) -> u32 {
     let a = 48271;
     let m = 2147483647;
-    (a * seed) % m
+    ((a * seed as u64) % m) as u32
+}
+
+fn miller_rabin_primality_test(testee: u32) -> bool {
+    println!("{}", testee);
+    let testee_one_off = testee - 1;
+
+    println!("{}", testee_one_off);
+    let two_factors = ((testee_one_off) as f32).log2() as u32;
+
+    println!("{}", two_factors);
+    let testee_one_off_twos_off = testee_one_off >> two_factors;
+
+    println!("{}", testee_one_off_twos_off);
+
+    let rounds = 10;
+
+    for k in (0..rounds) {}
+
+    true
 }
 
 fn main() {
@@ -18,4 +37,12 @@ fn main() {
 
     let seed = linear_congruential(seed);
     println!("Random number: {:032b}", seed);
+
+    let prime_test = miller_rabin_primality_test(seed);
+    println!("Is prime? : {}", prime_test);
+    let seed = linear_congruential(seed);
+    println!("Random number: {:032b}", seed);
+
+    let prime_test = miller_rabin_primality_test(seed);
+    println!("Is prime? : {}", prime_test);
 }
